141d12882b03673baa7972113409f466c187bedf
Rafel <r7121005846@gmail.com>
2023-10-15 20:50:28 -0600

    modal arreglado

--- main/resources/views/categories/index.blade.php
+++ main/resources/views/categories/index.blade.php
@@ -35,29 +35,29 @@
                 </div>
 
                 <div class="col-md-3 d-flex justify-content-end">
-                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modal-{{ $category->id }}" >Eliminar </button>
+                    <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modal-{{ $category->id }}">Eliminar </button>
                 </div>
-                
-              </div>
 
-              <div class="modal fade" id="modal-{{ $category->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" role="dialog">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="exampleModalLabel">{{$category->name}}</h1>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                      Al eliminar la categoria <strong>{{ $category->name }}</strong> se eliminaran todas las tareas qe esta dentro de esta.
-                      ¿Está seguro de borrar la categoria <strong>{{ $category->name }}</strong>?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No, cancelar</button>
-                        <form action="{{ route('categories.destroy',['category' => $category->id]) }}" method="POST">
-                            @method('DELETE')
-                            @csrf
-                            <button type="submit" class="btn btn-danger">Sí, eliminar</button>
-                        </form>
+
+                <div class="modal fade" id="modal-{{ $category->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" role="dialog">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">{{$category->name}}</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        Al eliminar la categoria <strong>{{ $category->name }}</strong> se eliminaran todas las tareas qe esta dentro de esta.
+                        ¿Está seguro de borrar la categoria <strong>{{ $category->name }}</strong>?
+                      </div>
+                      <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No, cancelar</button>
+                          <form action="{{ route('categories.destroy',['category' => $category->id]) }}" method="POST">
+                              @method('DELETE')
+                              @csrf
+                              <button type="submit" class="btn btn-danger">Sí, eliminar</button>
+                          </form>
+                      </div>
                     </div>
                   </div>
                 </div>
